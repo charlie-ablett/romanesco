c19d916766b8b516f24d15cd797770fec71029d0
ruth-ablett <ruthablett@gmail.com>
2014-06-10 10:00:43 +1200

    small cleanup in state_machine

--- lib/romanesco/state_machine.rb
+++ lib/romanesco/state_machine.rb
@@ -2,17 +2,6 @@ require 'romanesco/token'
 
 module Romanesco
 
-  TOKEN_TYPES = [
-      {name: 'addition', regex: /^\+/, token_class: AdditionToken},
-      {name: 'multiplication', regex: /^\*/, token_class: MultiplicationToken},
-      {name: 'subtraction', regex: /^\-/, token_class: SubtractionToken},
-      {name: 'division', regex: /^\//, token_class: DivisionToken},
-      {name: 'open parenthesis', regex: /^\(/, token_class: OpenParenthesisToken},
-      {name: 'close parenthesis', regex: /^\)/, token_class: CloseParenthesisToken},
-      {name: 'constant', regex: /^[0-9]*\.?[0-9]+/, token_class: ConstantToken},
-      {name: 'variable', regex: /^[a-zA-Z_]+/, token_class: VariableToken}
-  ]
-
   class ExpressionState
 
     def self.valid_finishing_state?
@@ -23,17 +12,13 @@ module Romanesco
       raise NotImplementedError
     end
 
-    def self.token_to_element(token)
-      token.element
-    end
-
     def self.transition(tree, tokens)
       if tokens.empty?
         raise InvalidExpressionError unless valid_finishing_state?
         return tree
       end
       current_token = tokens.shift
-      tree.add(token_to_element(current_token).new(current_token.expression_part)) unless current_token.is_a? CloseParenthesisToken
+      tree.add(current_token.element.new(current_token.expression_part)) unless current_token.is_a? CloseParenthesisToken
       tree.close_parenthesis if current_token.is_a? CloseParenthesisToken
       next_state = next_state(current_token)
       next_state.transition(tree, tokens)
